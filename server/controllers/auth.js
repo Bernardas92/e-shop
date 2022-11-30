c2c3310839184bdf3a07667e43c99d825b42f2da
Bernardas92 <Bernardas92@gmail.com>
2022-11-30 21:49:00 +0200

    server

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,7 @@
 import User from "../models/user.js";
-import { hashPassword, comparePassword } from "../helpers/auth.js"
+import { hashPassword, comparePassword } from "../helpers/auth.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
 export const register = async (req, res) => {
     try {
@@ -24,8 +26,61 @@ export const register = async (req, res) => {
             email,
             password: hashedPassword,
         }).save();
-        res.json(user);
+
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: '7d',
+        });
+
+        res.json({
+            user: {
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                address: user.address,
+            },
+            token,
+        });
     } catch (err) {
         console.log(err);
     }
+};
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email) {
+            return res.json({ error: "Email is taken" });
+        }
+        if (!password || password.length < 6) {
+            return res.json({ error: "Password must be at least 6 characters long" });
+        }
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.json({ error: "User not found" });
+        }
+        const match = await comparePassword(password, user.password);
+        if (!match) {
+            return res.json({ error: "Wrong password" })
+        }
+
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: '7d',
+        });
+
+        res.json({
+            user: {
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                address: user.address,
+            },
+            token,
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export const secret = async(req, res) => {
+    res.json({ currentUser: req.user });
 };
--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import morgan from "morgan";
 import authRoutes from "./routes/auth.js";
-
+import categoryRoutes from "./routes/category.js";
+import productRoutes from "./routes/product.js";
 
 dotenv.config();
 
@@ -11,7 +12,7 @@ const app = express();
 
 // db
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("db connected"))
+.then(() => console.log("DB Connected"))
 .catch((err) => console.log("DB ERROR => ", err));
 
 //middlewares
@@ -20,6 +21,8 @@ app.use(express.json());
 
 // router middleware
 app.use("/api", authRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
 
 const port = process.env.PORT || 8000;
 
